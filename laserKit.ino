7ff569e970bac53fab55c13d2dc1ea41030a3c10
Matt Canning <mcanning04@gmail.com>
2019-03-30 19:13:17 +0000

    Added two patterns for one touch fun

--- laserKit.ino
+++ laserKit.ino
@@ -6,70 +6,116 @@
 Servo xservo;
 Servo yservo;
 
-// Eastern
-int timezone = -4;
 
 char auth[] = "X";
 char ssid[] = "X";
 char pass[] = "X";
 
-bool workHours(){
-  
-  // Get current hour
-  time_t now = time(nullptr);
-  int hour = (now / 3600) % 24;
-  Serial.println("Current hour: " + hour);
-
-  // Check that it is between 9am and 5pm
-  if (hour > 8 && hour < 17){
-    return true;
-  } else {
-    return false;
-  }
-}
-
 // Laser on/off switch
 BLYNK_WRITE(V0){
 
-  // You can turn the laser off at any time of day
+  // Laser off
   if (param.asInt() == 0){
     digitalWrite(4, HIGH);
   }
-  // You can only turn it on between 9am and 5pm EST
+  // Laser on
   else if (param.asInt() == 1){
-    if (workHours() == 1){
-      digitalWrite(4, LOW);
-    } else {
-      Serial.println("Laser unavailable at this time of day");
-    }
+     digitalWrite(4, LOW);
   }
 }
 
 // Y Servo
 BLYNK_WRITE(V1){
-  if (workHours() == 1){
     digitalWrite(0, HIGH);
     yservo.write(param.asInt());
     delay(800);
     digitalWrite(0, LOW); 
-  } else {
-    Serial.println("Y servo unavailable at this time of day.");
-  }
 }
 
 // X Servo
 BLYNK_WRITE(V2){
-  if (workHours() == 1){
     digitalWrite(2, HIGH);
     xservo.write(param.asInt());
     delay(800);
     digitalWrite(2, LOW); 
-  } else {
-    Serial.println("X servo unavailable at this time of day.");
-  }
 }
 
-void setup(){ 
+// Pattern A
+BLYNK_WRITE(V3){
+    int xpos, rand_delay, elapsed_sec;
+
+    // Y Servo can be within range 127-175
+    digitalWrite(0, HIGH);
+    yservo.write(127);
+    delay(800);
+    digitalWrite(0, LOW);
+
+    // X servo on, laser on
+    digitalWrite(2, HIGH);
+    digitalWrite(4, LOW);
+
+    // Only run for 2 minute. (Function always runs twice for some reason)
+    time_t start = time(0);
+    elapsed_sec = difftime( time(0), start);
+    while( elapsed_sec <= 60 ) {
+      // X Servo can be within range 100-142
+      xpos = rand() % 43 + 100;
+      xservo.write(xpos);
+
+      // Delay between 500ms and 4000ms
+      rand_delay = rand() % 4000 + 500;
+      delay(rand_delay);
+
+      // Update elapsed time
+      elapsed_sec = difftime( time(0), start);
+    }
+
+    // X servo off, laser off
+    delay(1000);
+    digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+}
+
+// Pattern B
+BLYNK_WRITE(V4){
+    // X Servo can be within range 100-142
+    // Y Servo can be within range 127-175 (restricted to 140)
+    int elapsed_sec;
+    int y_op = -1, x_op = 1;
+    int xpos = rand() % 43 + 100;
+    int ypos = rand() % 10 + 127;
+
+    // X and Y servo on, laser on
+    digitalWrite(0, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(4, LOW);
+
+    // Only run for 2 minute. (Function always runs twice for some reason)
+    time_t start = time(0);
+    elapsed_sec = difftime( time(0), start);
+
+    while( elapsed_sec <= 60 ) {
+      // Does the movement, goes all the way to one direction and reverses
+      if( xpos <= 100 ){x_op = 1;} else if( xpos >= 142 ){x_op = -1;}
+      if( ypos <= 127 ){y_op = 1;} else if( ypos >= 150 ){y_op = -1;}
+      xpos = xpos + x_op;
+      ypos = ypos + y_op;
+
+      xservo.write(xpos);
+      yservo.write(ypos);
+      delay(15);
+      // Update elapsed time
+      elapsed_sec = difftime( time(0), start);
+    }
+
+    // X and Y servo off, laser off
+    delay(1000);
+    digitalWrite(0, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+}
+
+void setup(){
   // Debug output
   Serial.begin(115200);
   Serial.setDebugOutput(true);
@@ -86,9 +132,6 @@ void setup(){
   digitalWrite(0, LOW);
   digitalWrite(2, LOW);
   digitalWrite(4, HIGH);
-
-  //Configure time
-  configTime(timezone * 3600, 0, "pool.ntp.org", "time.nist.gov");
 
 }
 
